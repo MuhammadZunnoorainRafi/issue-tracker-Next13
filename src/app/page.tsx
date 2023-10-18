1eb710fbf921c34ed927477dde69d9a6f5fb3410
Muhammad Zunnoorain Rafi <bhoshyl@gmail.com>
2023-10-18 10:04:47 +0500

    laying out dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,11 +14,11 @@ export default async function Home() {
 
   return (
     <Grid columns={{ initial: '1', md: '2' }} gap="5">
-      {/* <Flex direction="column" gap="5">
+      <Flex direction="column" gap="5">
         <IssueSummary open={open} inProgress={inProgress} closed={closed} />
         <IssueChart open={open} inProgress={inProgress} closed={closed} />
       </Flex>
-      <LatestIssues /> */}
+      <LatestIssues />
     </Grid>
   );
 }
